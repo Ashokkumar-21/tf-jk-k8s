b00b4bbcbea80a40a94c684205b97d84cd8c2530
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-07-17 21:27:54 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-eks-terraform-state"      # your S3 bucket name
+    bucket         = "my-s3-bucket-new-tf-1"      # your S3 bucket name
     key            = "eks/terraform.tfstate"            # path inside bucket
     region         = "ap-south-1"
     dynamodb_table = "terraform-state-lock"             # use your existing table name
